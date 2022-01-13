4aa8d1822327e86041353d713112c4b21eb24d0f
SerpentSpirale <mathiasboulay@free.fr>
2022-01-13 17:31:31 +0100

    Add gles 320 in hardtext.c/h

--- src/glx/hardext.h
+++ src/glx/hardext.h
@@ -70,6 +70,7 @@ typedef struct _hardext {
     int glsl120;        // does version 120 glsl shader are supported ?
     int glsl300es;      // does version 300es glsl shader are supported ?
     int glsl310es;      // does version 300es glsl shader are supported ?
+    int glsl320es;      // VGPU specific ; does version 320es glsl shader are supported ?
 } hardext_t;
 
 EXPORT extern hardext_t hardext;
